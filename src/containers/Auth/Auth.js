fe9c9c64e1dd7e0415469a95c4d19fa57debf8f4
unknown <giachettimilena@gmail.com>
2020-08-09 20:27:56 -0300

    Add login form

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import classes from './Auth.module.scss';
+
+
+class Auth extends Component {
+    state = {
+        orderForm: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Your Password'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
+        },
+        formIsValid: false
+    }
+
+    checkValidity(value, rules) {
+        let isValid = true;
+        if(!rules) {
+            return true;
+        }
+        if(rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if(rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if(rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        if(rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid;
+        }
+
+        if(rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid;
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm,
+            [inputIdentifier]: {
+                ...this.state.orderForm[inputIdentifier],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+                touched: true
+            }
+        };
+        this.setState({orderForm: updatedOrderForm});
+    }
+
+    render () {
+        const formElementsArray = [];
+        for(let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        };
+
+        const form = formElementsArray.map(formElement => (
+            <Input
+                key={formElement.id}
+                elementType={formElement.config.elementType} 
+                elementConfig={formElement.config.elementConfig} 
+                value={formElement.config.value} 
+                invalid={!formElement.config.valid}
+                touched={formElement.config.touched}
+                shouldValidate={formElement.config.validation}
+                changed={(event) => this.inputChangedHandler(event, formElement.id)}
+            />
+        ))
+        return (
+            <div className={classes.Auth}>
+                <form>
+                    {form}
+                    <Button btnType="Success">Submit</Button>
+                </form>
+            </div>
+        );
+    }
+};
+
+export default Auth;
