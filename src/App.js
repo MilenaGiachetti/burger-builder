d56131daa38278c6eee0995efbd1997c47f9a1fb
unknown <giachettimilena@gmail.com>
2020-08-10 21:29:12 -0300

    Add logout display

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Checkout from './containers/Checkout/Checkout';
 import { Route } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
           <Route path="/orders" exact component={Orders} />
           <Route path="/auth" exact component={Auth} />
           <Route path="/checkout" component={Checkout} />
+          <Route path="/logout" component={Logout} />
         </Layout>
       </div>
     );
