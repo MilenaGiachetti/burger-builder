23201d59a24c9174ca3dfbcfc593249994d25a17
unknown <giachettimilena@gmail.com>
2020-08-19 20:54:13 -0300

    Add routing guards

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import { connect } from 'react-redux';
 import Checkout from './containers/Checkout/Checkout';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
@@ -18,20 +18,24 @@ class App extends Component {
       <Switch>
         <Route path="/auth" exact component={Auth} />
         <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
       </Switch>
     );
     if(this.props.isAuthenticated){
-
-
-    }
+      routes = (
+        <Switch>
+          <Route path="/orders" exact component={Orders} />
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    };
     return(
       <div>
         <Layout>
-          <Route path="/orders" exact component={Orders} />
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/auth" exact component={Auth} />
-          <Route path="/logout" component={Logout} />
-          <Route path="/" exact component={BurgerBuilder} />
+          {routes}
         </Layout>
       </div>
     );
